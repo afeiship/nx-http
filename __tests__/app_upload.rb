e2c695f47a8bd563b3fe1a09db6e94bf99737be4
afeiship <1290657123@qq.com>
2020-03-10 22:53:56 +0800

    add: weibo process script

--- __tests__/app_upload.rb
+++ __tests__/app_upload.rb
@@ -4,7 +4,19 @@ require_relative "../lib/nx-http"
 # p Nx::ContentType::JSON
 tempfile = open("https://tva1.sinaimg.cn/large/da432263gy1gcj74ifcokj205f06gjre.jpg")
 res = Nx::Http.upload("https://picupload.weibo.com/interface/pic_upload.php", { pic1: tempfile }) do |http, request|
-  request["Cookie"] = "SUB=_2A25zYkVLDeRhGeVI7VMZ8yzEyz-IHXVQFjGDrDV8PUNbmtAKLXLQkW9NTBpP0Tnizi_36CKgyQIO4Vzqxh9l_8Q2; Path=/; Domain=.weibo.com; HttpOnly"
+  request["Cookie"] = "SUB=_2A25zY-4vDeRhGeVI7VMZ8yzEyz-IHXVQGVjnrDV8PUNbmtAKLULmkW9NTBpP0XdxB7PNf9PhTe_HyO_VWEneHsw6; Path=/; Domain=.weibo.com; HttpOnly"
 end
 
-p res.body
+# hash = {
+#   "code" => "A00006",
+#   "data" => {
+#     "count" => 1, "data" => "eyJ1aWQiOjM2NjE4MzI4MDMsImFwcCI6Im1pbmlibG9nIiwiY291bnQiOjEsInRpbWUiOjE1ODM4NDk0MzUuNDE2LCJwaWNzIjp7InBpY18xIjp7IndpZHRoIjoxOTUsInNpemUiOjQxMjUsInJldCI6MSwiaGVpZ2h0IjoyMzIsIm5hbWUiOiJwaWNfMSIsInBpZCI6ImRhNDMyMjYzZ3kxZ2NwNnNkNmV6cmoyMDVmMDZnMHNrIn19fQ==",
+#     "pics" => {
+#       "pic_1" => {
+#         "width" => 195, "size" => 4125, "ret" => 1, "height" => 232, "name" => "pic_1",
+#         "pid" => "da432263gy1gcp6sd6ezrj205f06g0sk",
+#       },
+#     },
+#   },
+# }
+p JSON.parse res.body.split("\n")[2]
